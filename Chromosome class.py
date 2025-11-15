c940cc7cf72eb9f8c31c47e5c9420fdc87296dc4
sabrine-aouimeur <164254835+sabrine-aouimeur@users.noreply.github.com>
2025-11-15 17:26:05 +0100

    final chromosome class and ui without logic

---
+++ Chromosome class.py
@@ -0,0 +1,26 @@
+
+import random
+
+class Chromosome:
+
+    def __init__(self, genes=None):
+        
+         
+        if genes is not None :
+            self.genes = genes
+        else :   
+            self.genes = [random.randint(0, 7) for _ in range(63)]
+
+    def crossover(self, partner):
+        
+        point = random.randint(1, 62)   
+
+        child = self.genes[:point] + partner.genes[point:]
+
+        return Chromosome(child)
+
+    def mutate(self, mutation_rate=0.05):
+        
+        for i in range(len(self.genes)):
+            if random.random() < mutation_rate:
+                self.genes[i] = random.randint(0, 7)
